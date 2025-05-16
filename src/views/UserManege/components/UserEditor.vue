f242ff3aa233bddf282a6f185dd3f06c5ff4acc1
1022394845 <1022394845@qq.com>
2025-05-16 20:09:38 +0800

    编辑信息drawer

---
+++ src/views/UserManege/components/UserEditor.vue
@@ -0,0 +1,140 @@
+<script setup>
+import { modifyUserInfoAPI } from '@/api/user'
+import { Plus } from '@element-plus/icons-vue'
+
+const drawer = ref(false)
+
+const form = ref()
+const formModel = ref({})
+const rules = {
+  username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+  gender: [{ required: true, message: '请选择性别', trigger: 'blur' }],
+  phone: [
+    { required: true, message: '请输入电话号', trigger: 'blur' },
+    {
+      pattern: /^1[34578]\d{9}$/,
+      message: '请输入正确的手机号',
+      trigger: 'blur'
+    }
+  ],
+  email: [
+    { required: true, message: '请输入邮箱号', trigger: 'blur' },
+    {
+      pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      message: '请输入正确的邮箱号',
+      trigger: 'blur'
+    }
+  ],
+  password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
+}
+
+const open = (data) => {
+  if (form.value) form.value.resetFields()
+  formModel.value = { ...data }
+  drawer.value = true
+}
+defineExpose({ open })
+
+const imageFile = ref()
+const onChangeImage = (file) => {
+  imageFile.value = file
+  formModel.value.image = URL.createObjectURL(file.raw)
+}
+
+const submit = async () => {
+  await form.value.validate()
+  await modifyUserInfoAPI(formModel.value)
+  ElMessage.success('保存成功')
+  drawer.value = false
+}
+</script>
+
+<template>
+  <el-drawer v-model="drawer" size="25%">
+    <template #header>
+      <div class="title">{{ formModel.id ? '修改作者' : '新增作者' }}</div>
+    </template>
+    <template #default>
+      <el-form
+        ref="form"
+        :model="formModel"
+        :rules="rules"
+        label-width="auto"
+        hide-required-asterisk
+      >
+        <el-form-item label="账号" prop="account">
+          <el-input v-model="formModel.account" disabled />
+        </el-form-item>
+        <el-form-item label="名称" prop="username">
+          <el-input v-model="formModel.username" />
+        </el-form-item>
+        <el-form-item label="性别" prop="gender">
+          <el-radio-group v-model="formModel.gender">
+            <el-radio :value="1">男</el-radio>
+            <el-radio :value="2">女</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="电话" prop="phone">
+          <el-input v-model="formModel.phone" />
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="formModel.email" />
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="formModel.password" />
+        </el-form-item>
+        <el-form-item label="头像" prop="image">
+          <el-upload
+            class="avatar-uploader"
+            :show-file-list="false"
+            :auto-upload="false"
+            :on-change="onChangeImage"
+          >
+            <img v-if="formModel.image" :src="formModel.image" class="avatar" />
+            <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+          </el-upload>
+        </el-form-item>
+        <el-form-item label=" ">
+          <el-button @click="drawer = false">取消</el-button>
+          <el-button type="primary" @click="submit">确定</el-button>
+        </el-form-item>
+      </el-form>
+    </template>
+  </el-drawer>
+</template>
+
+<style scoped lang="scss">
+.title {
+  font-size: 16px;
+  font-weight: 700;
+}
+
+:deep(.avatar-uploader) {
+  .avatar {
+    display: block;
+    width: 100px;
+    height: 100px;
+  }
+
+  .el-upload {
+    border: 1px dashed var(--el-border-color);
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+    transition: var(--el-transition-duration-fast);
+
+    &:hover {
+      border-color: var(--el-color-primary);
+    }
+  }
+
+  .el-icon {
+    font-size: 28px;
+    width: 100px;
+    height: 100px;
+    color: #8c939d;
+    text-align: center;
+  }
+}
+</style>
--- src/views/UserManege/index.vue
+++ src/views/UserManege/index.vue
@@ -1,5 +1,105 @@
+<script setup>
+import { getUserListAPI } from '@/api/user'
+import { Plus, UserFilled, EditPen, Delete } from '@element-plus/icons-vue'
+import UserEditor from './components/UserEditor.vue'
+
+const pageInfo = ref({
+  page: 1, // 当前页码
+  pageSize: 10 // 页容量
+})
+// 用户列表
+const userNum = ref(0)
+const userList = ref([])
+const loading = ref(false)
+const getUserList = async () => {
+  loading.value = true
+  const {
+    data: { total, record }
+  } = await getUserListAPI(pageInfo.value)
+  userNum.value = total
+  userList.value = record
+  loading.value = false
+}
+onMounted(() => {
+  getUserList()
+})
+watch(
+  pageInfo,
+  () => {
+    getUserList()
+  },
+  {
+    deep: true
+  }
+)
+
+const userEditor = ref()
+const onEditUser = (data) => {
+  userEditor.value.open(data)
+}
+</script>
+
 <template>
   <div class="user-page">
-    <div class="operation"></div>
+    <div class="operation">
+      <el-button type="primary" :icon="Plus">添加作者</el-button>
+    </div>
+    <div class="table">
+      <el-table :data="userList" style="width: 100%" v-loading="loading">
+        <el-table-column prop="username" label="作者名称" min-width="40%" />
+        <el-table-column prop="image" label="作者头像" min-width="30%">
+          <template #default="{ row }">
+            <el-avatar fit="fill">
+              <img v-if="row.image" :src="row.image" alt="" />
+              <el-icon v-else><UserFilled /></el-icon>
+            </el-avatar>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" min-width="15%">
+          <template #default="{ row }">
+            <el-button type="primary" :icon="EditPen" @click="onEditUser(row)">编辑</el-button>
+            <el-button type="danger" :icon="Delete" @click="onDeleteUser(row.id)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页 -->
+      <div class="pagination" v-if="userNum">
+        <el-pagination
+          layout="prev, pager, next"
+          background
+          :total="userNum"
+          v-model:current-page="pageInfo.page_num"
+          v-model:page-size="pageInfo.page_size"
+        />
+      </div>
+    </div>
+    <UserEditor ref="userEditor" />
   </div>
 </template>
+
+<style scoped lang="scss">
+.user-page {
+  .operation {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    padding: 0 50px;
+    height: 80px;
+    border-radius: 10px;
+    background-color: #ffffff;
+  }
+
+  .table {
+    margin-top: 20px;
+    padding: 10px;
+    border-radius: 10px;
+    background-color: #ffffff;
+
+    .pagination {
+      margin-top: 30px;
+      display: flex;
+      justify-content: flex-end;
+    }
+  }
+}
+</style>
